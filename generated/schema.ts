68e085a0afaf7120af884d78cb9cf1edbc0bac69
Croooook <rajatgoyal6404@gmail.com>
2022-02-07 03:54:01 +0530

    added the new schema in the schema

--- generated/schema.ts
+++ generated/schema.ts
@@ -313,3 +313,65 @@ export class DepositTransferred extends Entity {
     this.set("newOwner", Value.fromBytes(value));
   }
 }
+
+export class TokenStakedInfo extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("owner", Value.fromBytes(Bytes.empty()));
+  }
+
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save TokenStakedInfo entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        "Cannot save TokenStakedInfo entity with non-string ID. " +
+          'Considering using .toHex() to convert the "id" to a string.'
+      );
+      store.set("TokenStakedInfo", id.toString(), this);
+    }
+  }
+
+  static load(id: string): TokenStakedInfo | null {
+    return changetype<TokenStakedInfo | null>(store.get("TokenStakedInfo", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get owner(): Bytes {
+    let value = this.get("owner");
+    return value!.toBytes();
+  }
+
+  set owner(value: Bytes) {
+    this.set("owner", Value.fromBytes(value));
+  }
+
+  get isDeposited(): boolean {
+    let value = this.get("isDeposited");
+    return value!.toBoolean();
+  }
+
+  set isDeposited(value: boolean) {
+    this.set("isDeposited", Value.fromBoolean(value));
+  }
+
+  get isStaked(): boolean {
+    let value = this.get("isStaked");
+    return value!.toBoolean();
+  }
+
+  set isStaked(value: boolean) {
+    this.set("isStaked", Value.fromBoolean(value));
+  }
+}
